86c665d14f41f1c0a0223e6a4023e0a1620239d6
Emerson Teixeira <emerson@exemplo.com>
2024-04-09 11:43:18 -0300

    terminando o projeto

--- tarefa-VueJS/src/App.vue
+++ tarefa-VueJS/src/App.vue
@@ -1,47 +1,100 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <header>
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <h1>Calculadora VueJS</h1>
+      <div class="wrapper">
+        <input type="number" v-model.number="numero1" @input="calcular" placeholder="numero 1">
+        <select v-model="operacao" @change="calcular">
+          <option value="+">soma (+)</option>
+          <option value="-">subtração (-)</option>
+          <option value="*">multiplicação (*)</option>
+          <option value="/">divizão (/)</option>
+        </select>
+        <input type="number" v-model.number="numero2" @input="calcular" placeholder="numero 2">
+      </div>
+    </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+      <div class="resultado">
+        <p>Resultado: {{ resultado }}</p>
+      </div>
+    </main>
+  </div>
 </template>
 
+<script setup>
+import { ref, computed } from 'vue';
+
+const numero1 = ref('');
+const numero2 = ref('');
+const operacao = ref('+');
+
+const resultado = computed(() => {
+  const n1 = parseFloat(numero1.value);
+  const n2 = parseFloat(numero2.value);
+
+  if (isNaN(n1) || isNaN(n2)) {
+    return '---';
+  }
+
+  switch (operacao.value) {
+    case '+':
+      return n1 + n2;
+    case '-':
+      return n1 - n2;
+    case '*':
+      return n1 * n2;
+    case '/':
+      if (n2 !== 0) {
+        return n1 / n2;
+      } else {
+        return 'Erro: divisão por zero';
+      }
+    default:
+      return 0;
+  }
+});
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .container {
+    margin-left: 20%;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  img {
+    margin-left: 50%;
+    width: 150px;
   }
-}
+
+  h1 {
+    margin-left: 20%;
+    font-size: 50px;
+    color: rgb(97, 18, 18);
+    text-align: center;
+  }
+
+  input {
+    font-size: 60px;
+  }
+
+  select {
+    margin-left: 40%;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    font-size: 20px;
+  }
+
+  .resultado {
+    text-align: center;
+    font-size: 40px;
+  }
 </style>
